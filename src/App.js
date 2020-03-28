74ca40229d23afcfead028bd369c21a24bfc2d04
sabbasachiPH <rentustat02@gmail.com>
2020-03-29 05:42:42 +0600

    Component Folder and Header Component added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React from "react";
-import logo from "./resources/logo2.png";
-import banner from "./resources/bannerbackground.png";
 import "./App.css";
-import ReactDOM from "react-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="header-right">
-          <FontAwesomeIcon icon={faShoppingCart} className="header-link" />
-          <a href="/" className="header-link">
-            Login
-          </a>
-          <a href="/" className="header-link signup">
-            Sign up
-          </a>
-          {/* <button className="btn btn-primary">Login</button>
-          <button className="btn btn-danger">Sign up</button> */}
-        </div>
-      </header>
-      <div className="banner">
-        <h1>Best food waiting for your belly</h1>
-        <div>
-          <input
-            type="text"
-            className="search"
-            placeholder="Search food items"
-          />
-          <button type="submit" className="signup">
-            Search
-          </button>
-        </div>
-      </div>
+      <Header></Header>
       <nav className="category">
         <a href="/breakfast">Breakfast</a>
         <a href="/launch">Launch</a>
